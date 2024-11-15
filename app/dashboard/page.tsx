bdd5b3d666b39fa0d6c033765e2279aba0a07836
Taha Polat <taha.polat@tgs.aero>
2024-11-15 15:21:30 +0300

    first commit from taha polat.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Dashboard } from "@/features/dashboard/components/table";
+
+export default function DashboardPage() {
+    return (
+        <div className="p-16">
+            <h1 className="text-4xl text-center">Live Data Dashboard</h1>
+            <Dashboard />
+        </div>
+    );
+};
---
+++ features/dashboard/components/table.tsx
@@ -0,0 +1,40 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+export function Dashboard() {
+    return (
+        <Table>
+            <TableCaption>Bir TGS Canlı Dashboard Örneği. </TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="w-[100px]">Invoice</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Method</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableCell className="font-medium"></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell className="text-right"></TableCell>
+                </TableRow>
+            </TableBody>
+            <TableFooter>
+                <TableRow>
+                    <TableCell colSpan={3}>Total</TableCell>
+                    <TableCell className="text-right">$2,500.00</TableCell>
+                </TableRow>
+            </TableFooter>
+        </Table>
+    )
+}
